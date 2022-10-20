7a89bfdadd8648a7d1701e26719244cc7eb30c20
ana-schoffen <ra115465@uem.br>
2022-10-20 18:25:25 -0300

    Telas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
+
 import { ReactElement } from 'react'
 import { Link } from 'react-router-dom'
 
 export function Home (): ReactElement {
   return (
     <div>
-      Home <Link to={'/livros'}>Livros</Link>
+      Home <Link to={'/livros'}>Livros</Link> <Link to={'/telacadastrofornecedor'}>Cadastro Fornecedor</Link>
     </div>
   )
 }
